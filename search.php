3f655717a651824d357dd5f62be22786d55e0227
Evan Mceldowney <evanmceldowney@gmail.com>
2018-11-08 20:57:48 -0800

    search fix

--- search.php
+++ search.php
@@ -30,7 +30,7 @@ get_header(); ?>
 			<?php if ( have_posts() ) : ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
-					<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+					<?php get_template_part( 'template-parts/content-simple-list', get_post_format() ); ?>
 				<?php endwhile; ?>
 
 				<?php else : ?>
